22dc1c125313b5caeced39bd0eb1b37e78f2b476
nokazn <nokazn@gmail.com>
2020-12-10 18:45:58 +0900

    backup when initializing

--- installer/init.sh
+++ installer/init.sh
@@ -2,12 +2,17 @@
 
 set nounset
 
-BASE_DIR="$(dirname $0)/.."
-DESTINATION=~/
+BASE_DIR="$(dirname ${0})/.."
+DESTINATION_DIR=~/
+BACKUP_DIR=~/dotfiles_backup
 counter=0
 
-# TODO 同名のファイルが既に存在すればバックアップする
-# function backup() {}
+# 同名のファイルが既に存在すればバックアップする
+function backup() {
+  if [[ -e $1 ]]; then
+    cp "$1" "${BACKUP_DIR}/$(basename ${1})"
+  fi
+}
 
 function increment_file_counter() {
   counter=$((counter+1))
@@ -25,14 +30,16 @@ for f in .??*; do
     ".git" | ".gitignore" )
       continue;;
     ".gitconfig.template" )
-      gitconfig="${DESTINATION}/.gitconfig"
-      cp $current $gitconfig
+      gitconfig="${DESTINATION_DIR}/.gitconfig"
+      backup $gitconfig
+      cp --verbose $current $gitconfig | sed -r -e "s/(^.*$)/✔ copied: \1/"
       # .gitconfig の email の箇所を置換
-      sed --in-place -e "2 s/email.*/email = $email/" $gitconfig
+      sed --in-place -e "2 s/email.*/email = ${email}/" $gitconfig
       increment_file_counter;;
     *)
+      backup "${DESTINATION_DIR}/${f}"
       # シンボリックリンクを作成
-      ln -snfv $current $DESTINATION
+      ln -snfv $current $DESTINATION_DIR | sed -r -e "s/(^.*$)/✔ linked: \1/"
       increment_file_counter;;
   esac
 done
