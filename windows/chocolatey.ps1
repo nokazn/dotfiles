312e5b3e8205fba149e4c76c18d091da45c351ba
nokazn <nokazn@gmail.com>
2020-12-13 19:01:44 +0900

    add chocolatey ps1 scripts

---
+++ windows/chocolatey.ps1
@@ -0,0 +1,41 @@
+if (!(Get-Command "choco" -ErrorAction SilentlyContinue)) {
+  # https://chocolatey.org/install.ps1 が安全かどうか検証し、インストールする
+  Set-ExecutionPolicy Bypass -Scope Process -Force;
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+
+  if (!(Get-Command "choco" -ErrorAction SilentlyContinue)) {
+    Write-Error "Chocolatey is not installed. You can get at https://chocolatey.org ."
+    exit 1;
+  }
+}
+
+choco install -Y googlechrome `
+firefox `
+brave `
+visualstudiocode `
+git `
+vcxsrf `
+# docker-desktop `
+pgadmin4
+postman `
+adobereader `
+keybase `
+discord `
+typora `
+boostnote `
+keepass `
+googledrive `
+everything `
+fastcopy `
+crystaldiskmark `
+crystaldiskinfo `
+7zip `
+gimp `
+vlc `
+monosnap `
+screentogif `
+spotify `
+lastfmscrobbler `
+todoist `
+slack
