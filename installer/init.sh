579d44547ed8f7bf157079ad590d08848a0be006
nokazn <nokazn@gmail.com>
2020-12-29 03:48:54 +0900

    fix invalid directory when backup

--- installer/init.sh
+++ installer/init.sh
@@ -3,41 +3,44 @@
 set nounset
 
 BASE_DIR="$(dirname ${0})/.."
-DESTINATION_DIR=~
-BACKUP_DIR=~/backup_dotfiles2020
+DESTINATION_BASE_DIR=~
+BACKUP_BASE_DIR=~/backup_dotfiles
 file_counter=0
 
-# @param {stirng} - destination file naem
-function backup_file() {
-  # シンボリックリンクの場合は上書きしてかまわないので無視
-  if [[ -L ${1} ]]; then
-    return
-  fi
+# @param {string} - destination path
+# @return {string}
+function show_overprompt_for_overwrite() {
+  read -rp "warning: ${1} already exists. Do you really want to overwrite? (Y/n) " response
+  echo ${response}
+}
 
-  file_name=$(basename ${1})
+# @param {stirng} - destination file path
+function backup() {
+  backup_file="${BACKUP_BASE_DIR}${1##${DESTINATION_BASE_DIR}}"
+  backup_dir="$(dirname ${backup_file})"
+
 # 同名のファイルが存在し、バックアップがなければ実行
-  if [[ -e ${1} ]]; then
-    if [[ !(-e "${BACKUP_DIR}/${file_name}") ]]; then
-      # バックアップ用のディレクトリがなければ作成
-      [[ !(-d ${BACKUP_DIR}) ]] && mkdir -p "${BACKUP_DIR}"
-      cp --verbose "${1}" "${BACKUP_DIR}/${file_name}"
-    fi
+  if [[ -f ${1} ]] && [[ !(-e ${backup_file}) ]]; then
+    # バックアップ用のディレクトリがなければ作成
+    [[ !(-d ${backup_dir}) ]] && mkdir -p "${backup_dir}"
+    cp --verbose "${1}" "${backup_file}" | sed --regexp-extended --expression "s/(^.*$)/✔ backuped: \1/"
   fi
 }
 
-# @param {stirng} - destination file naem
+# @param {stirng} - source  file path
+# @param {stirng} - destination file path
 function copy_file() {
-  # ログ出力の前にメッセージを表示
-  read -rp "warning: ${2} already exists. Do you really want to overwrite? (Y/n) " overwrite
-  if [[ $overwrite =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  response=$(show_overprompt_for_overwrite $1)
+  if [[ ${response} =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    backup "${2}"
     cp --verbose "${1}" "${2}" | sed --regexp-extended --expression "s/(^.*$)/✅ newly copied: \1/"
     increment_file_counter
   fi
 }
 
-# @param {string} - source
-# @param {string} - destination
-function link_file() {
+# @param {string} - source file path
+# @param {string} - destination file path
+function symbolic_link() {
   # すでにシンボリックリンクが存在する場合は無視
   if [[ -L ${2} ]]; then
     echo "📌 already linked: ${2}"
@@ -45,23 +48,21 @@ function link_file() {
   fi
 
   # destination にファイルが存在しないか、バックアップがある場合
-  if [[ !(-e ${2}) ]] || [[ -e "${BACKUP_DIR}/$(basename ${2})" ]]; then
+  if [[ !(-e ${2}) ]]; then
     ln --symbolic --verbose --force "${1}" "${2}" | sed --regexp-extended --expression "s/(^.*$)/✅ newly linked: \1/"
     increment_file_counter
   # ファイルが存在する場合
   elif [[ -f ${2} ]] && [[ -f ${1} ]]; then
-    # 上書きするか尋ねる
-    read -rp "warning: ${2} already exists. Do you really want to overwrite? (Y/n) " overwrite
-    if [[ $overwrite =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    response=$(show_overprompt_for_overwrite ${2})
+    if [[ ${response} =~ ^([yY][eE][sS]|[yY])$ ]]; then
+      backup "${2}"
       ln --symbolic --verbose --force "${1}" "${2}" | sed --regexp-extended --expression "s/(^.*$)/✅ newly linked: \1/"
       increment_file_counter
     fi
   # ディレクトリが存在する場合再帰的に呼び出す
   elif [[ -d ${1} ]] && [[ -d ${2} ]]; then
-    # TODO: cp: -r not specified; omitting directory '/home/nokazn/.vscode-server'
     while read -d ":" f; do
-    echo $2 $f
-      link_file ${f} ${2}/$(basename ${f})
+      symbolic_link ${f} ${2}/$(basename ${f})
     # Process Substitution (標準出力を仮のファイルに出力させる感じ)
     done < <(find ${1}/ -mindepth 1 -maxdepth 1 -printf "%p:")
     return
@@ -88,20 +89,18 @@ function main() {
         continue;;
 
       ".gitconfig.template" )
-        gitconfig="${DESTINATION_DIR}/.gitconfig"
-        backup_file "${gitconfig}"
+        gitconfig="${DESTINATION_BASE_DIR}/.gitconfig"
         copy_file "${current}" "${gitconfig}"
         # .gitconfig の email の箇所を置換
         sed --in-place --expression "2 s/email.*/email = ${email}/" "${gitconfig}";;
 
       *)
-        destination=${DESTINATION_DIR}/${f}
-        backup_file ${destination}
-        link_file ${current} ${destination};;
+        destination=${DESTINATION_BASE_DIR}/${f}
+        symbolic_link ${current} ${destination};;
     esac
   done
 
-  if [[ $? -eq 0 ]]; then
+  if [[ $? -eq 0 ]] && [[ $file_counter -gt 0 ]]; then
     echo "Successfully ${file_counter} dotfiles are initialized!"
   fi
 }
