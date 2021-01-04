65c19d3ca1044a18518e96d3b2afde13105219bb
nokazn <nokazn@gmail.com>
2021-01-04 19:37:26 +0900

    add uninstall_node script

---
+++ scripts/node/uninstall_node.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set nounset
+set errexit
+
+# @param {string} - command
+# @return {0|1}
+function has_command() {
+  type $1 >/dev/null 2>&1
+  return $?
+}
+
+# @param {string} - command
+# @return {0|1}
+function uninstall_node() {
+  if ! has_command nodenv; then
+    echo "nodenv doesn't exists."
+    return 0
+  fi
+
+  read -rp "Do you really want to uninstall nodenv? (Y/n) " response
+  if [[ ! $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    return 0;
+  fi
+
+  local nodenv_root=$(nodenv root)
+  rm -rf ${nodenv_root}
+  if has_command gaoenv; then
+    echo "❌ nodenv has been failed to uninstalled from '${nodenv_root}'."
+    return 1
+  fi
+  echo "✅ nodenv has been uninstalled successfully from '${nodenv_root}'!."
+  return 0
+}
+
+uninstall_node
+exit $?
