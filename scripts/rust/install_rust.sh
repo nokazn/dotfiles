f88369060dce96ab2ca2c8041a1a5f07ad3e43b2
nokazn <nokazn@gmail.com>
2021-01-05 16:45:12 +0900

    add install/uninstall rust script

---
+++ scripts/rust/install_rust.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set nounset
+set errexit
+
+function install_rust() {
+  echo "installing Rust ..."
+  # TODO: expect コマンドで自動化
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh "1"
+  if [[ $? -gt 0 ]]; then
+    echo "❌ Rust has been failed to install."
+    return 1
+  fi
+  # cargo のパスから /bin/cargo を除く
+  local cargo_path=$(which cargo | sed -e "s/\/bin\/cargo//")
+  local rustup_path=$(rustup show home)
+  echo "✅ Rust has been installed successfully at '${cargo_path}' and '${rustup_path}'!"
+  return 0
+}
+
+install_rust
+exit $?
---
+++ scripts/rust/uninstall_rust.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set nounset
+set errexit
+
+function uninstall_rust() {
+  if ! type "rustup" > /dev/null 2>&1; then
+    echo "❌ Rust ('rustup' command) doesn't exist."
+    return 0
+  elif ! type "cargo" > /dev/null 2>&1; then
+    echo "❌ Rust ('cargo' command) doesn't exist."
+    return 0
+  fi
+
+  # cargo のパスから /bin/cargo を除く
+  local cargo_path=$(which cargo | sed -e "s/\/bin\/cargo//")
+  local rustup_path=$(rustup show home)
+
+  echo "uninstalling Rust ..."
+  rustup self uninstall
+  echo "✅ Rust has been uninstalled successfully from '${cargo_path}' and '${rustup_path}'!"
+  return 0
+}
+
+uninstall_rust
+exit 0
