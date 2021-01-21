110effdb6b4d201d95a830bc10f154115ba98df4
nokazn <nokazn@gmail.com>
2021-01-21 22:23:53 +0900

    fix: UMC path error & add set options

--- scripts/start_vcxsrv.sh
+++ scripts/start_vcxsrv.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-readonly CURRENT_DIR=$(cd $(dirname ${0}); pwd)
-readonly CONFIG_PATH="${CURRENT_DIR}/../windows/config.xlaunch"
+set -o nounset
+set -o errexit
+set -o pipefail
+
+readonly BASE_DIR=$(cd "$(dirname ${0})/.."; pwd)
+readonly CONFIG_PATH="${BASE_DIR}/windows/config.xlaunch"
 
 # @param - none
 # @return {void}
@@ -12,8 +16,7 @@ function start_vcxsrv() {
     if [[ -z "$(tasklist.exe | grep vcxsrv)" ]]; then
       if [[ -f "${CONFIG_PATH}" ]]; then
         # wsl 内のパスを windows で有効なパスに変換してコマンドプロンプトから xlaunch を実行
-        # TODO: UNC path are not supported. と出力される
-        cmd.exe /c $(wslpath -w ${CONFIG_PATH})
+        cd /mnt/c; cmd.exe /c $(wslpath -w ${CONFIG_PATH}); cd $OLDPWD
       else
         echo "⚠ file 'config.xlaunch' doesn't exist at ${CONFIG_PATH}" >&2
       fi
