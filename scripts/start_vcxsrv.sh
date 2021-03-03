b74e459f1f10157754667f32269f509e90c5e7a7
nokazn <nokazn@gmail.com>
2021-03-03 16:48:52 +0900

    fix starting vcxsrv script

--- scripts/start_vcxsrv.sh
+++ scripts/start_vcxsrv.sh
@@ -11,9 +11,9 @@ readonly CONFIG_PATH="${BASE_DIR}/windows/config.xlaunch"
 # @return {void}
 function start_vcxsrv() {
   # WSL 内では X Server 経由で GUI を表示
-  if type wsl.exe > /dev/null 2>&1; then
-    # 実行中のプロセス一覧から vcxsrv を抽出した結果が空文字列で、config_path が存在する場合
-    if tasklist.exe | grep -q "vcxsrv"; then
+  if type wsl.exe >/dev/null 2>&1; then
+    # 実行中のプロセス一覧から vcxsrv を抽出した結果が0件で、config_path が存在する場合
+    if (( $(tasklist.exe | grep -c "vcxsrv") == 0 )); then
       if [[ -f "${CONFIG_PATH}" ]]; then
         # wsl 内のパスを windows で有効なパスに変換してコマンドプロンプトから xlaunch を実行
         cd /mnt/c; cmd.exe /c "$(wslpath -w "${CONFIG_PATH}")"; cd "$OLDPWD"
