1aebe5b103bf8b4007daa99a355a5e4251257275
nokazn <nokazn@gmail.com>
2021-06-12 18:34:32 +0900

    update: python installer

--- scripts/python/install_python.sh
+++ scripts/python/install_python.sh
@@ -22,65 +22,7 @@ function check_command() {
   return 0
 }
 
-# @param {string} - package name
-# @param {string?} - package location path
-# @return {void}
-function echo_success_message() {
-  if [[ $2 ]]; then
-    echo "✅ $1 has been installed successfully at '$2'!"
-  else
-    echo "✅ $1 has been installed successfully!"
-  fi
-  return 0
-}
-
-# @param {string} - package name
-# @param {string?} - package location path
-# @return {void}
-function echo_fail_message() {
-  if [[ $1 ]]; then
-    echo "❌ $1 has failed to be installed at '$2'."
-  else
-    echo "❌ $1 has failed to be installed."
-  fi
-  exit 1
-}
-
-# @param {string} - package name
-# @param {string} - package location path
-# @return {void}
-function echo_already_installed_message() {
-  echo "✅ $1 is already installed at '$2'."
-  return 0
-}
-
 # main -------------------------------------------------------------------------------
-
-# @param None
-# @return {void}
-function install_pyenv() {
-  local pyenv_path=~/.pyenv
-  if [[ -d ${pyenv_path} ]]; then
-    echo_already_installed_message "pyenv" ${pyenv_path}
-    return 0
-  elif [[ -e ${pyenv_path} ]]; then
-    echo "❌ The other package exists at '${pyenv_path}', but the path to pyenv doesn't exist."
-    exit 1
-  fi
-
-  echo "installing pyenv ..."
-  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-  # if ! (cd ~/.pyenv && src/configure && make -C src) >/dev/null 2>&1; then
-  #   echo "⚠ Failed to execute make or configure scripts."
-  # fi
-  # shellcheck disable=SC1090
-  source ${PATH_SCRIPT}
-  if ! has_command "pyenv" ${pyenv_path}; then
-    echo_fail_message "pyenv" ${pyenv_path}
-  fi
-  echo_success_message "pyenv" ${pyenv_path}
-  return 0
-}
 
 # @param None
 # @return {void}
@@ -90,7 +32,7 @@ function install_python() {
   if has_command "python"; then
     local python_version
     python_version=$(python --version | cut --delimiter " " -f 2)
-    echo_already_installed_message "Python ${python_version}" "$(which python)"
+    echo "Python ${python_version} is already installed at '$(which python)'."
     return 0
   fi
 
@@ -99,12 +41,14 @@ function install_python() {
   echo "installing Python the ${latest_version} (latest version of the major release) ..."
   pyenv install "${latest_version}"
   pyenv global "${latest_version}"
+  # shellcheck disable=SC1090
+  source ${PATH_SCRIPT}
   if ! has_command "python" || ! has_command "pip"; then
-    echo_fail_message "Python ${latest_version}"
+    echo "❌ Python ${latest_version} has failed to be installed."
+    exit 1
   fi
-  echo_success_message "Python ${latest_version}" "$(which python)"
+  echo "✅ Python ${latest_version} has been installed successfully at '$(which python)'!"
   return 0
 }
 
-install_pyenv
 install_python
