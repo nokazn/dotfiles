878312b42d639f01a47ef2f331b632801558c0f7
nokazn <41154684+nokazn@users.noreply.github.com>
2021-09-25 12:54:07 +0900

    [WIP] add: python packages

--- .config/nixpkgs/home/packages/python.nix
+++ .config/nixpkgs/home/packages/python.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }:
 
-with pkgs; [
-  pipenv
-]
+with pkgs;
+let
+  packages = [
+    pipenv
+    python39
+  ];
+  pythonPackages = with python39Packages; [
+    pip
+    setuptools
+  ];
+in
+packages ++ pythonPackages
