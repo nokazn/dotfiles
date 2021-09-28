84654489b861d022a500c78e93267921fd2a7952
nokazn <41154684+nokazn@users.noreply.github.com>
2021-09-28 17:54:38 +0900

    update: add git-crypt & git-lfs

--- .config/nixpkgs/home/packages/git.nix
+++ .config/nixpkgs/home/packages/git.nix
@@ -5,5 +5,7 @@ with pkgs; [
   gist # Gist lets you upload to https://gist.github.com/
   gitAndTools.gh # Work seamlessly with GitHub from the command line
   gitAndTools.delta # viewer for git and diff output
+  git-lfs # Git extension for versioning large files
+  git-crypt # Transparent file encryption in git
   pre-commit
 ]
