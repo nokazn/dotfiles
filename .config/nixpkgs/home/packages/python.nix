a36c427fba35e90d5b22a66e229baac5d63adc34
nokazn <41154684+nokazn@users.noreply.github.com>
2021-10-17 01:55:20 +0900

    add: python packages

--- .config/nixpkgs/home/packages/python.nix
+++ .config/nixpkgs/home/packages/python.nix
@@ -3,10 +3,14 @@
 with pkgs;
 let
   packages = [
+    black
     pipenv
+    poetry
     python39
   ];
   pythonPackages = with python39Packages; [
+    flake8
+    isort
     pip
     setuptools
   ];
