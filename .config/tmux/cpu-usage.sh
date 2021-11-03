ea5d3e1625cffb66c8a0af049cc0241e89c0a1c8
nokazn <41154684+nokazn@users.noreply.github.com>
2021-11-03 23:37:33 +0900

    update: cpu/memry usage in tmux status line

---
+++ .config/tmux/cpu-usage.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+function main() {
+  local -r available=$(mpstat | tail -n 1 | awk '{print $NF}')
+  local -r used=$(python -c "print(100 - ${available})")
+  echo "$(echo "${used}000" | cut -b 1-4)"%;
+}
+
+main
