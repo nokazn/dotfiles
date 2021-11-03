0816c9ca87ccbee9d83262ba733fbc58f8ee8408
nokazn <41154684+nokazn@users.noreply.github.com>
2021-11-04 00:03:39 +0900

    update: revoke VcXsrv for wslg environment

--- .sh_profile.sh
+++ .sh_profile.sh
@@ -2,11 +2,11 @@
 
 # ---------------------------------------- WSL  ----------------------------------------
 
-if type wslsys >/dev/null 2>&1; then
-    # WSL 内では X Server (VcXsrv) 経由で GUI を表示
+# WSLg 非対応の WSL 内では X Server 経由で GUI を表示
+if type wslsys >/dev/null 2>&1 && [[ ! -e /mnt/c/Windows/system32/wslg.exe ]]; then
     if [[ -f "$HOME/dotfiles/scripts/start_vcxsrv.sh" ]]; then
         # WSL に割り当てられる IP アドレスを取得して設定
-        DISPLAY="$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0"
+        DISPLAY="$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0"
         export DISPLAY
         "$HOME/dotfiles/scripts/start_vcxsrv.sh"
     else
--- scripts/start_vcxsrv.sh
+++ scripts/start_vcxsrv.sh
@@ -11,18 +11,15 @@ function start_vcxsrv() {
   local -r CONFIG_PATH="${BASE_DIR}/windows/config.xlaunch"
   local -r WIN32_PATH=/mnt/c/Windows/system32/
 
-  # WSL 内では X Server 経由で GUI を表示
-  if type wslsys >/dev/null 2>&1; then
-    # 実行中のプロセス一覧から vcxsrv を抽出した結果が0件で、config_path が存在する場合
-    if (( $("${WIN32_PATH}/tasklist.exe" | grep -c "vcxsrv") == 0 )); then
-      if [[ -f "${CONFIG_PATH}" ]]; then
-        # wsl 内のパスを windows で有効なパスに変換してコマンドプロンプトから xlaunch を実行
-        cd /mnt/c
-        "${WIN32_PATH}/cmd.exe" /c "$(wslpath -w "${CONFIG_PATH}")"
-        cd "$OLDPWD"
-      else
-        echo "⚠ file 'config.xlaunch' doesn't exist at ${CONFIG_PATH}" >&2
-      fi
+  # 実行中のプロセス一覧から vcxsrv を抽出した結果が0件で、config_path が存在する場合
+  if (( $("${WIN32_PATH}/tasklist.exe" | grep -c "vcxsrv") == 0 )); then
+    if [[ -f "${CONFIG_PATH}" ]]; then
+      # wsl 内のパスを windows で有効なパスに変換してコマンドプロンプトから xlaunch を実行
+      cd /mnt/c
+      "${WIN32_PATH}/cmd.exe" /c "$(wslpath -w "${CONFIG_PATH}")"
+      cd "$OLDPWD"
+    else
+      echo "⚠ file 'config.xlaunch' doesn't exist at ${CONFIG_PATH}" >&2
     fi
   fi
   return 0
