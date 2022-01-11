b4448c6add4f6fdfacc73aa36d986f38548c5a30
nokazn <41154684+nokazn@users.noreply.github.com>
2022-01-12 01:07:49 +0900

    fix: pre-commit & shellcheck errors

--- scripts/wsl-sudo-hello/install-wsl-sudo-hello.sh
+++ scripts/wsl-sudo-hello/install-wsl-sudo-hello.sh
@@ -6,17 +6,17 @@ function install_wsl_sudo_hello() {
     fi
 
     mkdir -p ~/downloads
-    cd ~/downloads;
+    cd ~/downloads || return 1
     if [[ ! -e ~/downloads/wsl-hello-sudo.tar.gz ]]; then
         wget http://github.com/nullpo-head/WSL-Hello-sudo/releases/latest/download/release.tar.gz -O wsl-hello-sudo.tar.gz
     fi
     mkdir -p ~/downloads/wsl-hello-sudo
     tar -xvf ~/downloads/wsl-hello-sudo.tar.gz -C ~/downloads/wsl-hello-sudo --strip-components 1
-    cd ~/downloads/wsl-hello-sudo
+    cd ~/downloads/wsl-hello-sudo || return 1
     # TODO: write failed 32: Broken pipe のエラーを無視するために標準エラー出力を捨てているため
     bash -c "yes | ./install.sh 2>/dev/null"
     echo "✅ WSL-Hello-sudo has been installed successfully!"
-    echo "👉 You need to add 'auth sufficient pam_wsl_hello.so' to the top line of your '/etc/pam.d/sudo'. See also https://github.com/nullpo-head/WSL-Hello-sudo/#configuration."    
+    echo "👉 You need to add 'auth sufficient pam_wsl_hello.so' to the top line of your '/etc/pam.d/sudo'. See also https://github.com/nullpo-head/WSL-Hello-sudo/#configuration."
     return 0
 }
 
