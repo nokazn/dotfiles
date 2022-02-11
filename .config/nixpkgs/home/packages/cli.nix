951cb44548da5bf316ab04afbbc3b939a807b02e
nokazn <41154684+nokazn@users.noreply.github.com>
2022-02-12 04:49:28 +0900

    feat: add powershell
    resolved by https://github.com/NixOS/nixpkgs/commit/c6c2dd3727e1c2d5b173653b4792f4ab4bd21d89

--- .config/nixpkgs/home/packages/cli.nix
+++ .config/nixpkgs/home/packages/cli.nix
@@ -21,6 +21,7 @@ with pkgs; let
     neofetch # CLI system information tool written in BASH that displays information about your system next to an image, your OS logo, or any ASCII file of your choice
     nkf # A Kanji code converter among networks
     pastel # A command-line tool to generate, analyze, convert and manipulate colors
+    powershell # Powerful cross-platform (Windows, Linux, and macOS) shell and scripting language based on .NET
     sampler # Tool for shell commands execution, visualization and alerting. Configured with a simple YAML file.
     shellcheck # A static analysis tool for shell scripts
     sl # SL(1): Cure your bad habit of mistyping
@@ -36,8 +37,6 @@ with pkgs; let
     wslu # A collection of utilities for Windows 10 Linux Subsystems
     wireshark
     yq # Command-line YAML processor - jq wrapper for YAML documents
-    # TODO: ビルドできない
-    # powershell
   ];
   alternative = [
     bat # A cat(1) clone with syntax highlighting and Git integration
