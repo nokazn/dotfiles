512356d48688be865d5ff099e0c064a53839214e
nokazn <41154684+nokazn@users.noreply.github.com>
2022-03-13 21:07:12 +0900

    refactor: rename start-vcxsrv scripts

--- .sh_profile.sh
+++ .sh_profile.sh
@@ -5,13 +5,13 @@
 if [[ "$(uname -r)" == *microsoft* ]]; then
     # WSLg 非対応の WSL 内では X Server 経由で GUI を表示
     if [[ ! -e /mnt/c/Windows/system32/wslg.exe ]]; then
-        if [[ -f "$HOME/dotfiles/scripts/start_vcxsrv.sh" ]]; then
+        if [[ -f "$HOME/dotfiles/scripts/start-vcxsrv.sh" ]]; then
             # WSL に割り当てられる IP アドレスを取得して設定
             DISPLAY="$(grep nameserver /etc/resolv.conf | awk '{print $2}'):0.0"
             export DISPLAY
-            "$HOME/dotfiles/scripts/start_vcxsrv.sh"
+            "$HOME/dotfiles/scripts/start-vcxsrv.sh"
         else
-            echo "⚠ file 'start_vcxsrv.sh' doesn't exist at ${START_VCXSRV_PATH}" >&2
+            echo "⚠ file 'start-vcxsrv.sh' doesn't exist at ${START_VCXSRV_PATH}" >&2
         fi
     fi
 fi
