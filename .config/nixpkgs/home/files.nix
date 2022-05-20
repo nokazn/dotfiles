23f90c71ef519e5ce13144adf7401a35bd89927b
nokazn <41154684+nokazn@users.noreply.github.com>
2022-05-20 22:30:54 +0900

    feat: add write permission to copied files

--- .config/nixpkgs/home/files.nix
+++ .config/nixpkgs/home/files.nix
@@ -2,13 +2,17 @@
 
 let
   files = builtins.readFile ./files.txt;
-  fileList = lib.splitString "\n" files;
+  fileList = builtins.filter (file: file != "") (lib.splitString "\n" files);
   fileSourceList = builtins.map
     (file: {
       name = file;
       value = {
         # https://discourse.nixos.org/t/how-to-refer-to-current-directory-in-shell-nix/9526
         source = builtins.toString ../../.. + ("/" + file);
+        target = file;
+        onChange = ''
+          chmod +w ${file}
+        '';
       };
     })
     fileList;
