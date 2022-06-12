836744fa6f5c084943997370b9dcf876d29021b8
nokazn <41154684+nokazn@users.noreply.github.com>
2022-06-12 11:21:19 +0900

    feat: add zoxide package

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -47,5 +47,6 @@ in
     starship = import ./programs/starship.nix { };
     tmux = import ./programs/tmux.nix { pkgs = pkgs; };
     vim = import ./programs/vim.nix { };
+    zoxide = import ./programs/zoxide.nix { };
   };
 }
