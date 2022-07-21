c9201090cc2c04ada05ddbf12efe3647fe7f9a24
nokazn <41154684+nokazn@users.noreply.github.com>
2022-07-22 07:32:20 +0900

    chore: add usage sub-command for deploy script

--- scripts/_deploy/windows.sh
+++ scripts/_deploy/windows.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-set -eu -o pipefail
+set -e -o pipefail
 
-ROOT_DIR=$(cd "$(dirname "${0}")/../../"; pwd)
+ROOT_DIR=$(cd "$(dirname "$0")/../../"; pwd)
 readonly ROOT_DIR
+
+function usage() {
+    cat <<EOF
+$(basename "$0") is a tool to deploy files to Windows environment
+
+Usage:
+    $(basename "$0") [command] [<options>]
+
+Options:
+    <default>         deploy Windows & WSL files
+    [files]           deploy Windows files
+    wsl               deploy WSL files
+    --help, -h        print this
+EOF
+}
 
 # cmd.exe 内でコマンドを実行してパスを取得し、UNIX のパスの形式に変換する
 function get-path-in-cmd() {
@@ -13,7 +28,7 @@ function get-path-in-cmd() {
 }
 
 # @param {string[]} - target files to deploy
-function main() {
+function deploy_user_files() {
   # プロセス置換して、一度 Windows 内のディレクトリに移動して %USERPROFILE% を出力する
   local -r destination_base_dir="$(
     tr -d "\r" < <(get-path-in-cmd "echo %USERPROFILE%")
@@ -24,11 +39,37 @@ function main() {
     "${destination_base_dir}" \
     "${destination_base_dir}/${BACKUP_DIR_NAME}" \
     "$@"
+}
 
-  # コピーするファイル名が指定されなかった場合にのみ実行
-  if [[ $# -eq 0 ]]; then
-    sudo cp --verbose "${ROOT_DIR}/wsl/etc/wsl.conf" /etc/wsl.conf
-  fi
+function deploy_wsl_files() {
+  sudo cp --verbose "${ROOT_DIR}/wsl/etc/wsl.conf" /etc/wsl.conf
+}
+
+function main() {
+  case $1 in
+    --help|-h)
+      usage
+    ;;
+
+    --debug)
+      # 実行するコマンドを出力
+      set -x
+      deploy_user_files "$@"
+    ;;
+
+    wsl)
+      deploy_wsl_files
+    ;;
+
+    "")
+      deploy_user_files "$@"
+      deploy_wsl_files
+    ;;
+
+    *)
+      deploy_user_files "$@"
+    ;;
+  esac
 }
 
 main "$@"
