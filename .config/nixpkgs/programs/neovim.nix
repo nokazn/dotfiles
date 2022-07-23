4408071940547d2dde66aecc29fe606120eadc03
nokazn <41154684+nokazn@users.noreply.github.com>
2022-07-23 19:18:02 +0900

    fix: unexported GPG_TTY environment variable

--- .config/nixpkgs/programs/neovim.nix
+++ .config/nixpkgs/programs/neovim.nix
@@ -1,6 +1,32 @@
 { pkgs, ... }:
 
 {
-  enable = true;
+  enable = false;
   package = pkgs.neovim-unwrapped;
+  viAlias = true;
+  vimAlias = true;
+  extraPackages = with pkgs; [
+    tree-sitter
+    rnix-lsp
+    nodePackages.typescript
+    nodePackages.typescript-language-server
+  ];
+  coc = {
+    enable = true;
+  };
+  withPython3 = true;
+  withNodeJs = true;
+
+  plugins = with pkgs; [
+    {
+      plugin = vimPlugins.gina-vim;
+    }
+    {
+      plugin = vimPlugins.fern-vim;
+    }
+  ];
+  extraConfig = ''
+    set undofile
+    set undodir=~/.vim/undodir
+  '';
 }
