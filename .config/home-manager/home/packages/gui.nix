7930c407c13764936bbe99120a90cbe693b2fd3e
nokazn <41154684+nokazn@users.noreply.github.com>
2024-01-07 11:58:44 +0900

    fix: resolve home-manager switch error

--- .config/home-manager/home/packages/gui.nix
+++ .config/home-manager/home/packages/gui.nix
@@ -2,6 +2,6 @@
 
 with pkgs; [
   # google-chrome
-  gnome.gedit # The default text editor of the GNOME desktop environment and part of the GNOME Core Applications
+  gedit # The default text editor of the GNOME desktop environment and part of the GNOME Core Applications
   xfce.thunar # A file manager for Linux and other Unix-like systems
 ]
