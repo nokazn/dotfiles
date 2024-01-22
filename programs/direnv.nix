e4988da081e3358b925f1105d80ed092a12c0e2b
nokazn <41154684+nokazn@users.noreply.github.com>
2024-01-22 09:58:07 +0900

    chore: add devshell settings

--- programs/direnv.nix
+++ programs/direnv.nix
@@ -2,6 +2,12 @@
 
 {
   enable = true;
+  enableBashIntegration = true;
+  enableZshIntegration = true;
+  config = {
+    load_dotenv = true;
+    strict_env = true;
+  };
   nix-direnv = {
     enable = true;
   };
