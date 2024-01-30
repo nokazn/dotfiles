ed5c5a07feae1b8bf14ccf6c5208f31a740b1f09
nokazn <41154684+nokazn@users.noreply.github.com>
2024-01-30 16:37:31 +0900

    chore: add salias as Go packages

--- programs/go.nix
+++ programs/go.nix
@@ -1,5 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   enable = true;
+  packages = {
+    "github.com/ktr0731/salias" = pkgs.fetchFromGitHub {
+      owner = "ktr0731";
+      repo = "salias";
+      rev = "v0.1.0";
+      hash = "sha256-OIUpDAISExfOZqoE1aXv5l8qBiOQyFk9CQVPrv0wydU=";
+    };
+  };
 }
