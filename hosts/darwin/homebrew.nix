1747c34d889650f4fb12f73eb3f3d5ccf1a72ab9
nokazn <41154684+nokazn@users.noreply.github.com>
2024-02-04 21:31:10 +0900

    feat: add nil as Nix's language

--- hosts/darwin/homebrew.nix
+++ hosts/darwin/homebrew.nix
@@ -1,6 +1,4 @@
-{ ... }:
-
-{
+{ ... }: {
   enable = true;
   taps = [
     "homebrew/cask-fonts"
--- modules/packages/nix.nix
+++ modules/packages/nix.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
-
 with pkgs; [
   alejandra
+  nil
   nixpkgs-fmt
   nixpkgs-lint
   nix-prefetch-git
