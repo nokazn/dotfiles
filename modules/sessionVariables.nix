790f3d0e647c24c3cca5b95ab84b34d4aa470f84
nokazn <41154684+nokazn@users.noreply.github.com>
2024-03-18 20:19:06 +0900

    chore: add default `tar` command options (-xvz)

--- modules/sessionVariables.nix
+++ modules/sessionVariables.nix
@@ -17,4 +17,6 @@
   # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
   # Remove -X and -F (exit if the content fits on one screen) to enable it.
   LESS = "-F -g -i -M -R -S -w -X -z-4";
+
+  TAR_OPTIONS = "-xvz";
 }
