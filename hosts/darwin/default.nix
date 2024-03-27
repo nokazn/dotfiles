87ef86fd25ec74196d6d65ba46a0632d1e55bcef
nokazn <41154684+nokazn@users.noreply.github.com>
2024-03-27 21:48:27 +0900

    chore: add tag trigger to build-macos workflow

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,4 +1,4 @@
-{ user, ... }:
+{ user, meta, ... }:
 
 {
   # necessary for nix-darwin
@@ -11,7 +11,7 @@
     };
   };
 
-  homebrew = import ./homebrew.nix { };
+  homebrew = if meta.isCi then { } else import ./homebrew.nix;
 
   system.defaults = import ./system.nix { };
 }
